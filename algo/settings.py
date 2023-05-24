42904ef716116fc99e72973a641e2c61de956f90
Agmir Hassane <hagmir6@gmail.com>
2023-05-24 19:09:35 +0100

    Add banners

--- algo/settings.py
+++ algo/settings.py
@@ -179,7 +179,7 @@ SUMMERNOTE_CONFIG = {
 
 
 
-if os.getcwd() == '/app':
+if not os.getcwd() == '/app':
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_ACCESS_KEY_ID = 'DO004CNRT4BAPJMAR7QH'
