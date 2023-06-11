d93cb7dc297872c852773a0e75e1ed18283a7f90
Agmir Hassane <hagmir6@gmail.com>
2023-06-11 21:19:38 +0100

    My commit

--- algo/settings.py
+++ algo/settings.py
@@ -193,7 +193,7 @@ if CPANEL:
             'ENGINE': 'django.db.backends.mysql',
             'NAME': os.getenv('DB_NAME'),
             'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWROD'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
             'HOST': os.getenv('DB_HOST'),
             'PORT': os.getenv('DB_PORT'),
             'OPTIONS': {
